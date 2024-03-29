7958f1fdf17d27a564780c8b7503398eeb1087e1
Danil Zhuravlov <danil.zhuravlov.2004@gmail.com>
2024-03-29 14:36:31 +0100

    added map

--- streamlit/app.py
+++ streamlit/app.py
@@ -18,10 +18,13 @@ cols = st.columns(3)
 # Address inputs organized in columns
 with cols[0]:
     street = st.text_input('Enter Street Name and Number', placeholder='Main Street 123')
+
 with cols[1]:
     city = st.text_input('Enter City Name', placeholder='Brussels')
+
 with cols[2]:
     zip_code = st.text_input('Enter Postal Code', value='1000')
+
     try:
         zip_code = int(zip_code)
         if not (0 <= zip_code <= 9999):
@@ -32,16 +35,35 @@ with cols[2]:
 
 country = st.text_input('Enter Country', placeholder='Belgium ')
 
-address = f'{street}, {city}, {zip_code}, {country}'
+# Check if all address fields are filled
+if street and city and zip_code and country:
 
-geolocator = Nominatim(user_agent="property_price_predictor")
-location = geolocator.geocode(address)
+    address = f'{street}, {city}, {zip_code}, {country}'
+    geolocator = Nominatim(user_agent="property_price_predictor")
+    location = geolocator.geocode(address)
+
+    if location:
+        latitude = location.latitude
+        longitude = location.longitude
+
+        # create df to display the map
+        map_data = pd.DataFrame({
+            'lat': [latitude],
+            'lon': [longitude]
+        })
+
+        # Display the map with the location in df
+        st.map(map_data, zoom=13)
+
+    else:
+        st.error('Address not found, please try again.')
+
 
 nbr_bedrooms = st.slider('Number of Bedrooms', 0, 15, 1)
 
 total_area_sqm = st.number_input(
     'Enter Total Area in Sqm',
-    value=0, # define start value
+    value=0, # define default value
     min_value=0,
     max_value=10000,
     step=5,
@@ -58,39 +80,38 @@ surface_land_sqm = st.number_input(
     )
 
 if st.button('Predict'):
-    # Transforms address to latitude and Longitude
-    geolocator = Nominatim(user_agent="property_price_predictor")
-    location = geolocator.geocode(address)
-    if location:
-        latitude = location.latitude
-        longitude = location.longitude
-        map_data = pd.DataFrame({
-             'lat': [latitude],
-             'lon': [longitude]
-             })
-        # Display the map with the location
-        st.map(map_data, zoom=16)
 
-        property_data = {
-            "total_area_sqm": total_area_sqm,
-            "surface_land_sqm": surface_land_sqm,
-            "latitude": latitude,
-            "longitude": longitude,
-            "nbr_bedrooms": nbr_bedrooms,
-            "zip_code": zip_code
-            }
+    if not (street and city and zip_code and country and nbr_bedrooms and total_area_sqm):
+        st.error('All fields are required to make a prediction.')
 
-        # Makes post request to the FastAPI endpoint
-        response = requests.post('https://property-price-predictor-405i.onrender.com/predict_price', json=property_data)
+    else:
+        address = f'{street}, {city}, {zip_code}, {country}'
+        geolocator = Nominatim(user_agent="property_price_predictor")
+        location = geolocator.geocode(address)
 
-        if response.status_code == 200:
-            prediction = response.json()
-            st.write(f'€{round(prediction["prediction"][0])}')
+        if location:
+            latitude = location.latitude
+            longitude = location.longitude
+            property_data = {
+                "total_area_sqm": total_area_sqm,
+                "surface_land_sqm": surface_land_sqm,
+                "latitude": latitude,
+                "longitude": longitude,
+                "nbr_bedrooms": nbr_bedrooms,
+                "zip_code": zip_code
+                }
+
+            # Makes post request to the FastAPI endpoint
+            response = requests.post('https://property-price-predictor-405i.onrender.com/predict_price', json=property_data)
+
+            if response.status_code == 200:
+                prediction = response.json()
+                st.write(f'€{round(prediction["prediction"][0])}')
+        
+            else:
+                st.error(f'Failed to get prediction from the API. Status code: {response.status_code}, Response: {response.text}')
         
         else:
-            st.error(f'Failed to get prediction from the API. Status code: {response.status_code}, Response: {response.text}')
-        
-    else:
-        st.write('Address not found, please try again.')
+            st.write('Address not found, please try again.')
 else:
     st.write('Enter values and click on the button')
